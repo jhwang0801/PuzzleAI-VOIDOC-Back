ba3981f6bec395aa17517bcbbc28df9a67b9a984
jaeseung <jhwang90801@gmail.com>
2022-06-27 17:53:06 +0900

    Modified(1): department, doctor list View
    - List Comprehension -> Concat & annotate 로 변경
    - user_name 리턴 값 제거, users/views.py 로그인 뷰에서 유저 id, name 데이터 리턴하도록 변경
    - 변경된 뷰에 따라서 테스트코드 변경 완료 / unittest 완료
    - Invalid_token 400 에러 -> 401에러 변경 완료

--- users/utils.py
+++ users/utils.py
@@ -34,7 +34,7 @@ def login_decorator(func):
             request.user = user
 
         except jwt.exceptions.DecodeError: 
-            return JsonResponse({'message' : 'INVALID_TOKEN'}, status = 400)
+            return JsonResponse({'message' : 'INVALID_TOKEN'}, status = 401)
         except CustomUser.DoesNotExist:
             return JsonResponse({'message' : 'INVALID_USER'}, status=400)
         except jwt.ExpiredSignatureError:
