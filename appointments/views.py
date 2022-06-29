d9dd76f74e33cfd9dca56f4363f4149025e61819
Andrew Choi <choiandrew585@gmail.com>
2022-06-29 13:09:15 +0900

    Add Calendar,TimeSlots View

--- appointments/views.py
+++ appointments/views.py
@@ -1,8 +1,11 @@
-from django.http           import JsonResponse
-from django.views          import View
-from django.conf           import settings
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from django.db.models.functions import Concat, Value as V
+from datetime import date
+from django.forms import DateField, TimeField
+from django.http                import JsonResponse
+from django.views               import View
+from django.conf                import settings
+from django.core.paginator      import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models           import CharField, Value as V
+from django.db.models.functions import Concat
 
 from users.utils  import login_decorator
 from users.models import Department, Doctor, WorkingDay, WorkingTime, CustomUser
@@ -12,39 +15,35 @@ from appointments.models import Appointment, UserAppointment
 class DepartmentsListView(View):
     @login_decorator
     def get(self, request):
+        departments_list = Department.objects.annotate(
+            thumbnails = Concat(V(f'{settings.LOCAL_PATH}/department_thumbnail/'), 'thumbnail', output_field=CharField())
+            ).values('id', 'name', 'thumbnails')
 
-        departments_list = [{
-            'id'       : department.id,
-            'name'     : department.name,
-            'thumbnail': settings.LOCAL_PATH + "department_thumbnail/" + f"{department.thumbnail}"
-        } for department in Department.objects.all()]
-
-        return JsonResponse({"result" : departments_list, "user_name" : request.user.name}, status = 200)
+        return JsonResponse({"result" : list(departments_list)}, status = 200)
 
 class DoctorListView(View):
     @login_decorator
     def get(self, request, department_id):
         try: 
-            page        = request.GET.get('page', 1)
-            doctors     = Doctor.objects.select_related('user', 'department', 'hospital').filter(department_id=department_id)
-            doctor_list = [{
-                'id'         : doctor.id,
-                'name'       : doctor.user.name,
-                'department' : doctor.department.name,
-                'hospital'   : doctor.hospital.name,
-                'profile_img': settings.LOCAL_PATH + "doctor_profile_img/" + f"{doctor.profile_img}"
-            } for doctor in doctors]
+            page    = request.GET.get('page', 1)
+            doctors = Doctor.objects.select_related('user', 'department', 'hospital').filter(department_id=department_id)\
+                .annotate(
+                    names        = Concat(V(''), 'user__name', output_field=CharField()),
+                    departments  = Concat(V(''), 'department__name', output_field=CharField()),
+                    hospitals    = Concat(V(''), 'hospital__name', output_field=CharField()),
+                    profile_imgs = Concat(V(f'{settings.LOCAL_PATH}/doctor_profile_img/'), 'profile_img', output_field=CharField())
+                ).values('id', 'names', 'departments', 'hospitals', 'profile_imgs').order_by('id')
 
-            doctors_paginator = Paginator(doctor_list, 6).page(page).object_list
+            doctors_paginator = Paginator(doctors, 6).page(page).object_list
 
-            return JsonResponse({"result" : doctors_paginator}, status=200)
+            return JsonResponse({"result" : list(doctors_paginator)}, status=200)
 
         except PageNotAnInteger:
             return JsonResponse({'message' : 'PAGE_HAS_TO_BE_AN_INTEGER'})
 
         except EmptyPage:
             return JsonResponse({'message' : 'THE_GIVEN_PAGE_CONTAINS_NOTHING'})
-"""
+
 class CalendarView(View):
     @login_decorator
     def get(self, request, doctor_id):
@@ -69,7 +68,7 @@ class TimeSlotsView(View):
         
         return JsonResponse({"result" : list(time_slots_list)}, status = 200)
 '''
-Class Appointment(models.Model):
+class Appointment(models.Model):
     symptom    = models.TextField()
     opinion    = models.TextField()
     state      = models.ForeignKey('State', on_delete=models.CASCADE)
@@ -78,7 +77,7 @@ Class Appointment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 '''
-
+"""
 class AppointmentView(View):
     @login_decorator
     def get(self, request):
