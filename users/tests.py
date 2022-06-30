714fc9272f5725b1e51b26d26893c27e83b13aaf
jaeseung <jhwang90801@gmail.com>
2022-06-30 17:16:48 +0900

    Improve view and test to be Object-Oriented(3)

--- users/tests.py
+++ users/tests.py
@@ -118,7 +118,7 @@ class SignUpIntegrityTest(TransactionTestCase):
             'message': "EMAIL_IS_ALREADY_REGISTERED"
         }) 
 
-class LoginTest(TestCase):
+class LoginTest(TestCase, Validation):
     def setUpTestData():
         CustomUser.objects.create_user(
             name      = 'kevin',
@@ -151,7 +151,7 @@ class LoginTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {
             'message'     : 'SUCCESS_PATIENT_LOGIN',
-            'access_token': Validation.generate_jwt(user),
+            'access_token': self.generate_jwt(user),
             'user_id'     : user.id,
             'user_name'   : user.name
         })
@@ -185,7 +185,7 @@ class LoginTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {
             'message'     : 'SUCCESS_LOGIN',
-            'access_token': Validation.generate_jwt(user),
+            'access_token': self.generate_jwt(user),
             'user_id'     : user.id,
             'user_name'   : user.name
         })
@@ -204,7 +204,7 @@ class LoginTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {
             'message'     : 'SUCCESS_LOGIN',
-            'access_token': Validation.generate_jwt(doctor),
+            'access_token': self.generate_jwt(doctor),
             'user_id'          : doctor.id,
             'user_name'        : doctor.name
             }
