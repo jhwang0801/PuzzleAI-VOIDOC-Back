b390dbc8acc6cf7687a381ebaad177955a5a073d
jaeseung <jhwang90801@gmail.com>
2022-06-30 16:28:36 +0900

    Improve view and test to be Object-Oriented
    - Move validate_password into SignUpView Class
    - Add Validation Class
    - Move check_duplicates and generate_jwt into Validation Class

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from users.views import SignUpView, LoginView,CheckDuplicateEmailView
+from users.views import SignUpView, LoginView, CheckDuplicateEmailView
 
 urlpatterns = [
     path('/signup', SignUpView.as_view()),
     path('/login', LoginView.as_view()),
-    path('/check_duplicate', CheckDuplicateEmailView.as_view()),
-
+    path('/check_duplicate', CheckDuplicateEmailView.as_view())
 ]
--- users/utils.py
+++ users/utils.py
@@ -1,29 +1,9 @@
-import re
 import jwt
 
-from datetime               import datetime, timedelta
-
-from django.core.exceptions import ValidationError
-from django.db.utils        import IntegrityError
-from django.conf            import settings
-from django.http            import JsonResponse
+from django.conf import settings
+from django.http import JsonResponse
 
 from users.models import CustomUser
-
-def validate_password(password):
-    REGEX_PASSWORD = '^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,}$'
-
-    if not re.match(REGEX_PASSWORD, password):
-        raise ValidationError('Enter a valid password.')
-
-def check_duplicate_email(email):
-    if CustomUser.objects.filter(email = email).exists():
-        raise IntegrityError
-
-def generate_jwt(user):
-    payload      = {'user_id': user.id, 'exp': datetime.now() +timedelta(hours=2)}
-    access_token = jwt.encode(payload, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
-    return access_token
 
 def login_decorator(func):
     def wrapper(self,request,*args,**kwargs):
