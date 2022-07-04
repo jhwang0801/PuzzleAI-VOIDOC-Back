45c9abc591c685d77442896da3cd215135f0852a
jaeseung <jhwang90801@gmail.com>
2022-07-04 15:44:07 +0900

    ADD CancellationView for the function of appointment cancellation
    - add test code
    - add exceptions

--- users/utils.py
+++ users/utils.py
@@ -1,11 +1,10 @@
 import jwt
 
-from datetime               import datetime, timedelta
+from datetime import datetime, timedelta
 
-from django.conf import settings
-from django.http import JsonResponse
-from django.db.utils        import IntegrityError
-
+from django.conf     import settings
+from django.http     import JsonResponse
+from django.db.utils import IntegrityError
 
 from users.models import CustomUser
 
@@ -18,6 +17,30 @@ class Validation:
         payload      = {'user_id': user.id, 'exp': datetime.now() +timedelta(hours=2)}
         access_token = jwt.encode(payload, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
         return access_token
+
+class DateTimeFormat:
+    def format_date_time(self, date, time):
+        if date.weekday() == 0:
+            day = "(월) "
+        elif date.weekday() == 1:
+            day = "(화) "
+        elif date.weekday() == 2:
+            day = "(수) "
+        elif date.weekday() == 3:
+            day = "(목) "
+        elif date.weekday() == 4:
+            day = "(금) "
+        elif date.weekday() == 5:
+            day = "(토) "
+        elif date.weekday() == 6:
+            day = "(일) "
+
+        if time.strftime("%p") == "AM":
+            time_format = "오전 "
+        elif time.strftime("%p") == "PM":
+            time_format = "오후 "
+        
+        return f'{date.strftime("%Y-%m-%d")}{day}{time_format}{time.strftime("%-I:%M")}'
 
 def login_decorator(func):
     def wrapper(self,request,*args,**kwargs):
