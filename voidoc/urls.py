e0f90513a84a9a48da94a09412a4f54e5a43ff14
jaeseung <jhwang90801@gmail.com>
2022-07-06 14:31:40 +0900

    ADD docker file to EC2 for deploy
    - fix all merge conflicts
    - add docker file
    - update debug setting to False

--- voidoc/urls.py
+++ voidoc/urls.py
@@ -1,7 +1,8 @@
-from django.urls               import path, include
+from django.urls               import path, include, re_path
 from django.contrib            import admin
 from django.conf               import settings
 from django.conf.urls.static   import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,5 +10,11 @@ urlpatterns = [
     path('appointments', include('appointments.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT
+    }),
+]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
