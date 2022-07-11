d615c38086798856945e90979c949ef37225c3d1
jaeseung <jhwang90801@gmail.com>
2022-07-11 16:46:28 +0900

    ADD video call function
    - Add consumers.py to build signaling server
    - Confirm websocket connection in Local

--- voidoc/asgi.py
+++ voidoc/asgi.py
@@ -2,19 +2,16 @@ import os
 import videocalls.routing
 
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
+from channels.auth    import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voidoc.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(
-                videocalls.routing.websocket_urlpatterns
-            )
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            videocalls.routing.websocket_urlpatterns
         )
     ),
 })
