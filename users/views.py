54be2bab80acc8cda1729580d368c57463188e92
Andrew Choi <choiandrew585@gmail.com>
2022-07-22 16:10:57 +0900

    Add ActivateUserView for user activation to use account Modify SignupView, Login View to reflect this change in sending an activation email

--- users/views.py
+++ users/views.py
@@ -4,9 +4,15 @@ from django.views           import View
 from django.http            import JsonResponse
 from django.db.utils        import IntegrityError
 from django.forms           import ValidationError
+from django.core            import mail
 from django.core.validators import validate_email
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth    import login, authenticate
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site  
+from django.template.loader import render_to_string  
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
 from users.models import CustomUser
 from users.utils  import Validation
@@ -24,12 +30,32 @@ class SignUpView(View, Validation):
             self.validate_password(password)
             self.check_duplicate_email(email)
 
-            CustomUser.objects.create_user(
+            user = CustomUser.objects.create_user(
                 name      = name,
                 email     = email,
                 password  = password,
-                is_doctor = is_doctor
+                is_doctor = is_doctor,
             )
+            
+            connection = mail.get_connection()
+            connection.open()
+            current_site = get_current_site(request)  
+            message = render_to_string('acc_active_email.html', {  
+                'name': name,  
+                'domain': current_site.domain,  
+                'uid': urlsafe_base64_encode(force_bytes(user.id)),  
+                'token': default_token_generator.make_token(user),  
+            })
+            email = mail.EmailMessage(
+                'Welcome, ' + name + ', to VOIDOC!',
+                message,
+                'noreply.voidoc@gmail.com',
+                [email],
+                connection=connection,
+            )
+            connection.send_messages([email])
+            connection.close()
+
             return JsonResponse({'message' : 'SUCCESS'}, status=201)
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
@@ -39,6 +65,20 @@ class SignUpView(View, Validation):
             return JsonResponse({'message' : str(e)}, status=400)
         except IntegrityError:
             return JsonResponse({'message' : 'EMAIL_IS_ALREADY_REGISTERED'}, status=400)
+
+class ActivateUserView(View, Validation):
+    def get(self, request, uid, token):
+        try:
+            id = force_str(urlsafe_base64_decode(uid))
+            user = CustomUser.objects.get(pk=id)  
+        except(TypeError, ValueError, OverflowError, CustomUser.DoesNotExist):  
+            user = None
+        if user is not None and default_token_generator.check_token(user, token):  
+            user.is_active = True  
+            user.save()  
+            return JsonResponse({'message' : 'SUCCESS_USER_ACTIVATED'}, status=200)  
+        else:  
+            return JsonResponse({'message' : 'INVALID_AUTHORIZATION_LINK'}, status=400)    
 
 class LoginView(View, Validation):
     def post(self, request):
@@ -77,7 +117,11 @@ class LoginView(View, Validation):
             else:
                 return JsonResponse({'message' : 'INVALID_TYPE_OF_APPLICATION_ON_HEADER'}, status=400)
         else:
-            return JsonResponse({'message' : 'WRONG_EMAIL_OR_PASSWORD'}, status=401)
+            user = CustomUser.objects.get(email=email)
+            if(user.is_active == False):
+                return JsonResponse({'message' : 'USER_NOT_YET_ACTIVATED'}, status=401)
+            else:
+                return JsonResponse({'message' : 'WRONG_EMAIL_OR_PASSWORD'}, status=401)
 
 class CheckDuplicateEmailView(View, Validation):
     def post(self, request):
